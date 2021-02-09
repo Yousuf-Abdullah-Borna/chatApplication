c30e54d3a972f852fe3b89e687db66c603079f06
Yousuf Borno <yb.26.code@gmail.com>
2021-02-09 05:38:08 -0500

    All configured for the chatapplication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
 const app = express();
+const socketio = require('socket.io')
+
+const server= http.createServer(app)
+
+const io = socketio(server);
+
 
 const port = process.env.PORT || 8000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+//helps to serve the static files
 app.use(express.static(publicDirectoryPath))
 
-app.listen( port, ()=>{
+
+io.on('connection',()=>{
+
+    console.log("connection is alive")
+
+})
+
+server.listen( port, ()=>{
 
     console.log("server is ready")
 })
